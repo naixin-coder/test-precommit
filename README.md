6b07f7ddef2cd9d3e7ea84da04f8452867f7db7c
naixin-coder <liulin_suyan@163.com>
2022-02-17 11:18:19 +0800

    remove debugger

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test-precommit
+
 Husky Eslint
---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: 刘林
+ * @Date: 2022-02-17 09:55:45
+ * @LastEditors: 刘林
+ * @LastEditTime: 2022-02-17 11:17:22
+ */
+
+console.log('hello')
